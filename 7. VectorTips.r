12dee413cfaf61cd4ca5735bb4434eae0937089c
svkosev <svkosev@svet-vb>
2017-06-05 17:58:02 +0300

    Lesson 7 Awesome Vector Tips

---
+++ 7. VectorTips.r
@@ -0,0 +1,26 @@
+#Awsome Vector Tips
+
+#Fast Initializing of a List
+#<firstNumber>:<lastNumber>
+
+myList <- 1:5
+myList
+
+largeList <- 1:1000
+largeList
+
+negNums <- -3:-12
+negNums
+
+#Operations on vectors of equal length
+a <- 1:3
+b <- 7:9
+a
+b
+a+b
+
+#Checking length of a vector
+#using length() function
+length(a)
+length(b)
+a*b
