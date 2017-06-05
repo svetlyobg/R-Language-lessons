f498d433a108b6d2cbd60f2d14a7b1065f23db31
svkosev <svkosev@svet-vb>
2017-06-05 17:57:35 +0300

    Lesson 6 Vectors/Lists

---
+++ 6. Vectors.r
@@ -0,0 +1,18 @@
+# Vectors
+
+# List called b1
+b1 <- c(1,2,3,4,5) 
+
+#List called b2
+b2 <- c("svetlozar","vladkov","kosev")
+b1
+b2
+
+# multiply list b1 by 2. No need for loop cycle
+b1*2
+
+#multiply b1 list by power of 3
+b1 ^ 3
+
+#Suaqre Root
+sqrt(b1)
