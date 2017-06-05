487714f281857771ece6eeaf1b918c545b74c655
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-06-05 15:05:30 +0300

    Data Types

---
+++ 1. Intro - Assign Operators.R
@@ -0,0 +1,32 @@
+# assigning value to variable
+# by operator =
+a = 3
+a+20
+
+
+# assigning value to variable
+# by operator <-
+
+b <- 8 
+sum <- b + 10
+
+# multiple variables can be assigned to 
+# the same value
+
+svetlyo <- krasen <- 23
+svetlyo
+krasen
+
+# assigning value to variable
+# via assign function
+
+assign("myVar", 10)
+myVar *9
+
+# remove assigned value of variable
+# it uses rm function
+
+assign("removeThis", 60)
+removeThis
+rm(removeThis)
+removeThis
---
+++ 2. DataTypes.R
@@ -0,0 +1,10 @@
+# Data type
+
+number <- 23
+name <- "svetlyo"
+someVar = "23"
+
+# function class prints data type of variable
+class(number)
+class(name)
+class(someVar)
