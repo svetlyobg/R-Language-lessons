eb8e079ac7901cc61584ced60eecbb8b26b5009b
svkosev <svkosev@svet-vb>
2017-06-05 17:57:12 +0300

    Lesson 5 Logical Operators

--- 5. LogicalOperators.r
+++ 5. LogicalOperators.r
@@ -1,4 +1,28 @@
 # Logic Operators
 # == is equal?
-
 5==7
+5==5
+
+# not equal
+5 != 7
+5 != 5
+
+# less
+5 < 7
+7 < 5
+
+#greater
+5>7
+7>5
+
+# less or equal
+5 <= 7
+7 <= 5
+
+#greater or equal
+5 >= 7
+7 >= 5
+
+# comparing strings
+"svetlozar" == "kosev"
+"svetlozar" == "svetlozar"
