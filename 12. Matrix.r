c9caed29ca9e899925ebb6dea5e140c5c68d9d5c
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-06-06 14:30:24 +0300

    Lesson 12 Matrix

---
+++ 12. Matrix.r
@@ -0,0 +1,16 @@
+#Matrix
+#one <- matrix(data, nrow = rows, ncol = cols)
+one <- matrix(1:100, nrow = 10)
+one
+
+two <- matrix(51:60, nrow = 2)
+three <- matrix(61:70, nrow = 2)
+two
+three
+
+#return number of rows & cols
+dim(two)
+dim(three)
+
+#multiplication of matrices
+two*three
